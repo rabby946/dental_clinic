0003e7e2c625080da4472a48c8ed98ea48dd1fb4
Fahim Ahmed Rabbi <rabbi35-946@diu.edu.bd>
2026-01-14 19:22:56 +0600

    add whitenoise

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 # ----------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
